c6521a670f5343594b76b6b577810e868f80c560
Jamie Rollinson <joff1979@gmail.com>
2023-05-07 12:07:39 +0000

    Syling Changes

--- whataline-remix/app/routes/_index.tsx
+++ whataline-remix/app/routes/_index.tsx
@@ -53,18 +53,29 @@ export default function Index() {
                 </div>
           </nav>
           <div className="flex justify-normal mx-auto max-w-7xl">
-            <a href="#" className="flex flex-col items-center m-6 bg-pink-500 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-[#E45286] dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img className="object-cover w-full h-96 md:h-auto md:w-40 md:rounded-none m-5  border-gray-200 border-4" src={livelong} alt="Live Long"/>
-                <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-sans font-bold tracking-tight text-gray-900 dark:text-white">Live Long</h5>
-                    <p className="mb-3 font-normal font-sans text-gray-700 dark:text-gray-400">Trapped in a temporal loop following a fatal accident, a beleaguered florist makes the ultimate sacrifice to free herself and save her soulmate from repeated death.</p>
-                </div> 
+            <a href="#" className="m-6 bg-pink-500 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-[#E45286] dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <div className="md:flex-row md:max-w-xl flex flex-col items-start">
+                  <img className="object-cover w-full h-96 md:h-auto md:w-40 md:rounded-none m-5  border-gray-200 border-4" src={livelong} alt="Live Long"/>
+                  <div className="flex flex-col justify-between p-4 leading-normal">
+                      <h5 className="mb-2 text-2xl font-serif font-bold tracking-tight text-white dark:text-white px-4">
+                        Live Long</h5>
+                      <p className="mb-3 font-normal font-sans text-white dark:text-gray-400 px-4">Trapped in a temporal loop following a fatal accident, a beleaguered florist makes the ultimate sacrifice to free herself and save her soulmate from repeated death.</p>
+                  </div> 
+                </div>
+                <div className="flex justify-center w-full divide-x divide-blue-200 pb-4 text-white">
+                  <p className="px-4">Feature</p>
+                  <p className="px-4">Romance</p>
+                  <p className="px-4">Dramedy</p>
+                </div>
             </a>
-            <a href="#" className="block max-w-xl p-6 m-6 bg-[#ECEBEC] border hover:bg-[#ECEBEC] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 className="mb-2 text-2xl font-bold font-serif tracking-tight text-gray-900 dark:text-white">Welcome to Kat's world</h5>
+            <a href="#" className="flex flex-col items-start max-w-xl m-6 bg-[#ECEBEC] border hover:bg-[#ECEBEC] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <div className="max-w-s flex p-4">
+                  <hr className="h-0.5 bg-black w-10 my-auto inline-block mr-4"></hr>
+                  <h5 className="inline-block text-2xl font-bold font-serif tracking-tight text-gray-900 dark:text-white">
+                    Welcome to Kat's world</h5>
+                </div>
                 <p className="font-normal font-serif  text-gray-700 dark:text-gray-400">Sometimes funny, sometimes messy. Always entertaining.
-                <br/><br/>
-
+                <br/><br/> 
 Kat Rollinson is an award-winning screenwriter who continues to amass an impressive portfolio of features, pilots and shorts. She specialises in poignant comedy, romance and drama, often with a twist of magical realism. Her
 flair for creating quirky storylines and layered characters has resulted in incredible feedback, which has acknowledged her organic, charming, masterful storytelling that oozes full of life.</p>
             </a>
